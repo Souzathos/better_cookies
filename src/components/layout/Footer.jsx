1df68ab2515536db2f9c4f15230972a717db3303
Athos <athosfelipe01@gmail.com>
2026-04-13 17:56:21 -0300

    feat: add new better cookies logo

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,4 +1,4 @@
-import logo from '../../assets/logo.jpg'
+import logo from '../../assets/logo.png'
 import { WhatsAppIcon, InstagramIcon } from '../ui/icons'
 import { generateWhatsAppLink, generalOrderMessage } from '../../utils/whatsapp'
 import { siteConfig } from '../../data/siteConfig'
